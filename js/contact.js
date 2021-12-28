8e2b5e1446ea52f0b901677870244dcc186e8329
Emii91 <69647535+Emii91@users.noreply.github.com>
2021-12-28 11:18:06 +0100

    fixed contact form

--- js/contact.js
+++ js/contact.js
@@ -1,62 +1,62 @@
-// const form = document.querySelector("#contactForm");
-// const fullName = document.querySelector("#name");
-// const email = document.querySelector("#email");
-// const address = document.querySelector("#address");
-// const subject = document.querySelector("#subject");
-// const success = document.querySelector(".success");
+const form = document.querySelector("#contactForm");
+const fullName = document.querySelector("#name");
+const email = document.querySelector("#email");
+const address = document.querySelector("#address");
+const subject = document.querySelector("#subject");
+const success = document.querySelector(".success");
 
-// function validateForm() {
-//     event.preventDefault();
+function validateForm() {
+    event.preventDefault();
 
-//     if(checkLength(fullName.value, 0) === true) {
-//         fullNameError.style.display = "none";
-//     }
-//     else {
-//         fullNameError.style.display = "block";
-//     }
+    if(checkLength(fullName.value, 0) === true) {
+        fullNameError.style.display = "none";
+    }
+    else {
+        fullNameError.style.display = "block";
+    }
 
-//     if(checkLength(address.value, 35) === true) {
-//         addressError.style.display = "none";
-//     }
-//     else {
-//         addressError.style.display = "block";
-//     }
+    if(checkLength(address.value, 35) === true) {
+        addressError.style.display = "none";
+    }
+    else {
+        addressError.style.display = "block";
+    }
 
-//     if(checkLength(subject.value, 10) === true) {
-//         subjectError.style.display = "none";
-//     }
-//     else {
-//         subjectError.style.display = "block";
-//     }
+    if(checkLength(subject.value, 10) === true) {
+        subjectError.style.display = "none";
+    }
+    else {
+        subjectError.style.display = "block";
+    }
 
-//     if(validateEmail(email.value) === true) {
-//         emailError.style.display = "none";
-//     }
-//     else {
-//         emailError.style.display = "block";
-//     }
+    if(validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    }
+    else {
+        emailError.style.display = "block";
+    }
 
-//     console.log("Relevant input");
-// }
+    console.log("Relevant input");
+}
 
-// form.addEventListener("submit", validateForm);
+form.addEventListener("submit", validateForm);
 
-// function checkLength(value, len) {
-//     if(value.trim().length > len) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
+function checkLength(value, len) {
+    if(value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// function validateEmail(email) {
-//     const regEx = /\S+@\S+\.\S+/;
-//     const patternMatches = regEx.test(email);
-//     return patternMatches;
-// }
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
 
-// function submit(event) {
-//     form.reset();
-// }
+function submit(event) {
+    form.reset();
+}
 
-// form.addEventListener("submit", submit);
+form.addEventListener("submit", submit);
