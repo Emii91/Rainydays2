353216af7e4b01c3eb4430447179e61e1a6ab95c
Emii91 <69647535+Emii91@users.noreply.github.com>
2022-01-02 12:21:48 +0100

    small changes

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -8,7 +8,7 @@ for (var i = 0; i < removeItem.length; i++) {
         pClicked.parentElement.parentElement.remove();
         // updateTotal();
 
-        cart.innerHTML = `<h2>Shoppingcart is empty</h2>
+        cart.innerHTML = `<h2 class="h2-js">Shoppingcart is empty</h2>
                           <a href="index.html">Go back</a>`;
     })
 };
