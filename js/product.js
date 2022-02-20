33485a50ced08f1015bee4a2fec2cba319f34f9d
Emii91 <69647535+Emii91@users.noreply.github.com>
2022-02-20 20:19:19 +0100

    done api

--- js/product.js
+++ js/product.js
@@ -1,29 +1,31 @@
 const resultsContainer = document.querySelector(".results");
 
-const url = "http://localhost/rainydays/wp-json/wp/v2/pages";
+const url = "https://flower-power.store/Rainydays/wp-json/wc/store/products";
 
 async function fetchProducts() {
 
-try {
-    const response = await fetch(url);
-    const json = await response.json();
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
 
-    console.log(json);
+        console.log(json);
 
-    resultsContainer.innerHTML = "";
+        resultsContainer.innerHTML = "";
 
-    const newarrivals = json.results;
+        for (let i = 0; i < json.length; i++) {
 
-    newarrivals.forEach(function(newarrivals) {
-        resultsContainer.innerHTML += ``
+            resultsContainer.innerHTML += ` <a href="newarrivals-item.html?id=${json.id}" class="results">
+                                            <img src="${json.images}" />
+                                            <div class="name">${json[i].name}</div>
+                                            <p class="description">${json[i].description}</p>
+                                            <p class="price">${json[i].prices.price}</p>
+                                            <button class="add-to-cart">${json[i].add_to_cart.text}</button> 
+                                            </a>`;
+        }                      
 
-    });
-
-} catch (error) {
-    console.log("An error has occurred");
-    resultsContainer.innerHTML =  displayError("An error has occurred");
+  } catch (error) {
+        console.log(error);
    }
-
 }
 
 fetchProducts();
